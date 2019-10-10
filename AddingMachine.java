895dae55358c5ff6764c28735d0db92dfcfbb76b
Trey-Anderson-24 <55422680+Trey-Anderson-24@users.noreply.github.com>
2019-10-09 17:33:15 -0700

    Add files via upload

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,4 +1,5 @@
 package cse360assign2;
+
 /*
  * Takes the input and can either add or subtract it from the total while the 
  * toString prints out the inputs and whether it is a addition or subtraction
@@ -6,38 +7,49 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String endString = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
+	
 	/*
 	 * returns the total running value
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
+	
 	/*
 	 * @parm takes in a input value
 	 * adds the input value to total to kepp a running total
 	 */
 	public void add (int value) {
-		
+		total = total + value;
+		endString = endString + " + " + value;
 	}
+	
 	/*
 	 * @parm takes a input value
 	 * subtracts the value from the running total
 	 */
 	public void subtract (int value) {
-		
+		total = total - value;
+		endString = endString + " - " + value;
 	}
+	
 		/*
 		 * returns the inputs and whether they were added or subtracted
 		 */
 	public String toString () {
-		return "";
+		return endString;
 	}
-
-	public void clear() {
 	
+	/*
+	 * method clear returns the total to 0 and makes the endString to 0
+	 */
+	public void clear() {
+	endString  = "0";
+	total = 0;
 	}
 }
